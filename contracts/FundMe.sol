0e6cbacc58264cd430bc00c34fe482aebcf3c450
luke-holt <luke.holt55@gmail.com>
2021-11-22 15:15:36 -0400

    first commit

---
+++ contracts/FundMe.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.6;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
+
+contract FundMe {
+    using SafeMathChainlink for uint256;
+
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+    address public owner;
+    AggregatorV3Interface public priceFeed;
+
+    constructor(address _priceFeed) public {
+        priceFeed = AggregatorV3Interface(_priceFeed);
+        owner = msg.sender;
+    }
+
+    function fund() public payable {
+        // $50
+        uint256 minimumUSD = 50 * 10**18;
+        // 1 gwei < $50
+        require(
+            getConversionRate(msg.value) >= minimumUSD,
+            "You need to spend more ETH!"
+        );
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
+        // what the ETH -> USD conversion rate
+    }
+
+    function getVersion() public view returns (uint256) {
+        // the address here is the address to the contract containing the conversion rate for ETH -> USD
+        return priceFeed.version();
+    }
+
+    function getPrice() public view returns (uint256) {
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        return uint256(answer * 10000000000);
+        // Number format: x,xxx.xxxxxxxx
+        // By multiplying the answer by 1e10, the returned value is in wei.
+    }
+
+    // 1 gwei: 10000000000
+    function getConversionRate(uint256 ethAmount)
+        public
+        view
+        returns (uint256)
+    {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
+        return ethAmountInUsd;
+        // 0.000043324779876900 (1 gwei in usd)
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        // minimumUSD
+        uint256 minimumUSD = 50 * 10**18;
+        uint256 price = getPrice();
+        uint256 precision = 1 * 10**18;
+        return (minimumUSD * precision) / price;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    // this is only callable by the owner
+    function withdraw() public payable onlyOwner {
+        // only want the contract admin/owner
+        msg.sender.transfer(address(this).balance);
+
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        funders = new address[](0);
+    }
+}
